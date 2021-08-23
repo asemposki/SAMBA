7e29aa4b76535a855bcc8ef030bea1ae8de3f264
Alexandra Semposki <as727414@ohio.edu>
2021-08-23 10:42:43 -0400

    added truncated prior

--- priors.py
+++ priors.py
@@ -15,10 +15,24 @@ class Priors:
         pass
 
     
+    def luniform(self, theta, g):
+
+        '''
+        General uniform prior to be used to truncate the normal
+        distributions used in the parameter priors. 
+        '''
+
+        if theta > g:
+            return 0.0
+        else:
+            return -np.inf
+
+    
     def lpdf(self, params):
 
         '''
-        Log pdf of the priors for the parameters.
+        Log pdf of the priors for the parameters. Must be truncated
+        for the sampler to walk in valid regions. 
         '''
 
         if len(params) == 2:
@@ -29,11 +43,16 @@ class Priors:
 
         elif len(params) == 3:
 
-            param_1 = stats.norm.logpdf(params[0], 0.11, 0.01)
-            param_2 = stats.norm.logpdf(params[1], 0.2, 0.01)
-            param_3 = stats.norm.logpdf(params[2], 0.16, 0.01)
+            #g1 truncated between (0, inf)
+            g1 = self.luniform(params[0], 0.0) + stats.norm.logpdf(params[0], 0.12, 0.1)
 
-            return (param_1 + param_2 + param_3)
+            #g3 truncated between (g1, inf)
+            g3 = self.luniform(params[2], g1) + stats.norm.logpdf(params[2], 0.16, 0.1)
+
+            #g2 truncated between (g3, inf)
+            g2 = self.luniform(params[1], g3) + stats.norm.logpdf(params[1], 0.2, 0.1)
+
+            return (g1 + g2 + g3)
 
         else:
             raise ValueError('The number of parameters does not match any available switching function.')
