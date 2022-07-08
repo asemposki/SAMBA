df0ca3c0e05692c0b637ab95b384c153cbd1c0b6
Alexandra Semposki <as727414@ohio.edu>
2022-07-08 18:36:39 -0400

    finished simple tests

--- Tests/test_GP_BMM.py
+++ Tests/test_GP_BMM.py
@@ -12,6 +12,7 @@ from contextlib import contextmanager
 
 #matplotlib settings for Latex plots
 import matplotlib
+from sympy import N
 matplotlib.rcParams.update({
     'font.family': 'serif',
     'text.usetex': True,
@@ -52,34 +53,78 @@ def test_GP(g, ns, nl, expectation):
 #call the training() function from the GP() class
 obj_tr1 = obj1.training(error=True, method=2, plot=True)
 
-#TODO: check return object
+##
+#check return object
+def test_training(expectation):
+    with expectation:
+        assert obj1.training() is not None
+##
 
 #call the validate() function
 mean1, sig1, cov1 = obj1.validate(plot=True)
 
-#TODO: check mean1, sig1, cov1
+##
+#check mean1, sig1, cov1
+def test_validate(expectation):
+    with expectation:
+        assert mean1 is not None 
+        assert sig1 is not None 
+        assert cov1 is not None 
+##
 
 #call plot_mix() to mix in the GP
-mixed_mean, mixed_intervals = obj1.plot_mix(g, plot_fdagger=True, plot_true=True, GP_mean=mean1, GP_var=np.square(sig1))
+mixed_mean, mixed_intervals = obj1.plot_mix(g, plot_fdagger=True, \
+    plot_true=True, GP_mean=mean1, GP_var=np.square(sig1))
 
-#TODO: check returned variables
+##
+#check returned variables
+def test_plot_mix(expectation):
+    with expectation:
+        assert mixed_mean is not None
+        assert mixed_intervals is not None
+##
 
 #call subplot_mix() and plot no GP results next to GP results
 obj1.subplot_mix(g, GP_mean=mean1, GP_var=np.square(sig1))
 
-#TODO: check figure generation
+##
+#check figure generation
+def test_subplot_mix(g, expectation):
+    with expectation:
+        assert obj1.subplot_mix(g, GP_mean=mean1, GP_var=np.square(sig1)) \
+            is not None
+##
 
 #calculate the Mahalanobis points
 md_g, md_mean, md_sig, md_cov = obj1.MD_set(pts=3, plot=True)
 
-#TODO: check each output
+##
+#check each output
+def test_MD_set(expectation):
+    with expectation:
+        assert md_g is not None 
+        assert md_mean is not None 
+        assert md_sig is not None 
+        assert md_cov is not None 
+##
 
 #use the points to calculate the Mahalanobis distance for our GP
 md_gp, md_ref = obj1.md_squared(md_g, md_mean, md_cov, n_curves=1000)
 
-#TODO: check return output
+##
+#check return output
+def test_md_squared(expectation):
+    with expectation:
+        assert md_gp is not None 
+        assert md_ref is not None 
+##
 
 #call 
 obj1.md_plotter(md_gp, md_ref, hist=True)
 
-#TODO: check plotter for axes generation
+##
+#check plotter for axes generation
+def test_md_plotter(md_gp, md_ref, expectation):
+    with expectation:
+        assert obj1.md_plotter(md_gp, md_ref) is not None
+##
