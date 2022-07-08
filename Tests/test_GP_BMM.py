2b6d2592124118b5c61aaf1c9eba5d842d64519c
Alexandra Semposki <as727414@ohio.edu>
2022-07-08 16:44:02 -0400

    added more tests

--- Tests/test_GP_BMM.py
+++ Tests/test_GP_BMM.py
@@ -31,23 +31,55 @@ nl = 3
 #instantiate GP object for N_s = N_l = 3
 obj1 = GP(g, ns, nl, kernel="Matern", nu=1.5, ci=68, error_model='informative')
 
+##
+#check ns, nl, g passing
+@contextmanager
+def does_not_raise():
+    yield
+
+@pytest.mark.parametrize(
+    "g, ns, nl, expectation",
+    [(g, ns, nl, does_not_raise()),],
+)
+
+def test_GP(g, ns, nl, expectation):
+    with expectation:
+        assert GP.gpredict == g 
+        assert GP.loworder == ns
+        assert GP.highorder == nl
+##
+
 #call the training() function from the GP() class
 obj_tr1 = obj1.training(error=True, method=2, plot=True)
+
+#TODO: check return object
 
 #call the validate() function
 mean1, sig1, cov1 = obj1.validate(plot=True)
 
+#TODO: check mean1, sig1, cov1
+
 #call plot_mix() to mix in the GP
 mixed_mean, mixed_intervals = obj1.plot_mix(g, plot_fdagger=True, plot_true=True, GP_mean=mean1, GP_var=np.square(sig1))
+
+#TODO: check returned variables
 
 #call subplot_mix() and plot no GP results next to GP results
 obj1.subplot_mix(g, GP_mean=mean1, GP_var=np.square(sig1))
 
+#TODO: check figure generation
+
 #calculate the Mahalanobis points
 md_g, md_mean, md_sig, md_cov = obj1.MD_set(pts=3, plot=True)
+
+#TODO: check each output
 
 #use the points to calculate the Mahalanobis distance for our GP
 md_gp, md_ref = obj1.md_squared(md_g, md_mean, md_cov, n_curves=1000)
 
+#TODO: check return output
+
 #call 
 obj1.md_plotter(md_gp, md_ref, hist=True)
+
+#TODO: check plotter for axes generation
