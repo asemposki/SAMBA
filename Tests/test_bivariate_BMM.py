141e469f726f314df000137d0eecb8c149a8f73c
Alexandra Semposki <as727414@ohio.edu>
2022-07-08 12:19:29 -0400

    added some tests

--- Tests/test_bivariate_BMM.py
+++ Tests/test_bivariate_BMM.py
@@ -1,9 +1,12 @@
-##simple example to run pytest with Bivariate BMM method
+###########################################################
+# Tutorial example for Bivariate BMM
+###########################################################
 
 #import packages
 import numpy as np
 import math
 import pytest
+from contextlib import contextmanager
 import statistics
 from scipy import stats, special, integrate
 
@@ -27,30 +30,62 @@ ns = 2
 nl = 2
 
 #instantiate first object for N_s = N_l = 2
-model1 = Bivariate(ns, nl, error_model='informative')
+error_model='informative'
+model1 = Bivariate(ns, nl, error_model)
 
-#assert that model1 is not None (unless these are NoneType objects?)
+
+#test class object
+@contextmanager
+def does_not_raise():
+    yield
+
+@pytest.mark.parametrize(
+    "ns, nl, error_model, expectation",
+    [(ns, nl, error_model, does_not_raise()),],
+)
+
+def test_Bivariate(ns, nl, error_model, expectation):
+    with expectation:
+        assert Bivariate(ns, nl, error_model) is not None
+
 
 #call plot_models() from Models() class
 model1.plot_models(g)
 
-#assert that this is not None (creates axes), check ns, nl passing
+#TODO: check ns, nl passing
+
+#check figure generation
+def test_plot_models(g, expectation):
+    with expectation:
+        assert model1.plot_models(g) is not None
+
 
 #call plot_mix() function to mix
 mean1, intervals1 = model1.plot_mix(g, plot_fdagger=True)
 
-#check mean and interval and see what is going on inside here (check ns, nl passing)
+#TODO: check mean and interval and see what is going on inside here (check ns, nl passing)
 
-## start a new test
 
 #instantiate a new object for N_s = N_l = 5
 ns = 5
 nl = 5
-model2 = Bivariate(ns, nl, error_model='informative')
+error_model = 'informative'
+model2 = Bivariate(ns, nl, error_model)
 
-#check model2 type
+
+#check class object again
+@pytest.mark.parametrize(
+    "ns, nl, error_model, expectation",
+    [(ns, nl, error_model, does_not_raise()),],
+)
+
+def test_Bivariate(ns, nl, error_model, expectation):
+    with expectation:
+        assert Bivariate(ns, nl, error_model) is not None
+
 
 #call plot_mix()
 mean2, intervals2 = model2.plot_mix(g, plot_fdagger=True)
 
-#again check mean2, interval2, and ns, nl passing 
+#TODO: again check mean2, interval2
+#TODO: again check ns, nl passing 
