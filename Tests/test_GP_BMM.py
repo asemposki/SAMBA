d191c3057bafc67bdaef53eebfecc3a2ab9fd84f
Alexandra Semposki <as727414@ohio.edu>
2022-07-10 19:03:09 -0400

    finished these tests

--- Tests/test_GP_BMM.py
+++ Tests/test_GP_BMM.py
@@ -4,72 +4,61 @@
 
 #import packages
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.ticker import AutoMinorLocator
-
 import pytest
-from contextlib import contextmanager
 
 #matplotlib settings for Latex plots
 import matplotlib
-from sympy import N
 matplotlib.rcParams.update({
     'font.family': 'serif',
     'text.usetex': True,
 })
 
 #import the SAMBA classes needed
-from samba.models import Models, Uncertainties
-from samba.discrepancy import Bivariate
 from samba.gaussprocess import GP
-from samba.fprdat import FPR
 
 #define g and series expansions
 g = np.linspace(1e-6,1.0,200)
-ns = 3
-nl = 3
+ns = np.array([3])
+nl = np.array([3])
 
 #instantiate GP object for N_s = N_l = 3
 obj1 = GP(g, ns, nl, kernel="Matern", nu=1.5, ci=68, error_model='informative')
 
 ##
 #check ns, nl, g passing
-@contextmanager
-def does_not_raise():
-    yield
+# @contextmanager
+# def does_not_raise():
+#     yield
 
-@pytest.mark.parametrize(
-    "g, ns, nl, expectation",
-    [(g, ns, nl, does_not_raise()),],
-)
+# @pytest.mark.parametrize(
+#     "g, ns, nl, expectation",
+#     [(g, ns, nl, does_not_raise()),],
+# )
 
-def test_GP(g, ns, nl, expectation):
-    with expectation:
-        assert GP.gpredict == g 
-        assert GP.loworder == ns
-        assert GP.highorder == nl
+def test_GP():
+    assert np.array_equal(obj1.gpredict, g) 
+    assert np.array_equal(obj1.loworder, ns)
+    assert np.array_equal(obj1.highorder, nl)
 ##
 
 #call the training() function from the GP() class
-obj_tr1 = obj1.training(error=True, method=2, plot=True)
+obj_tr1 = obj1.training(error=True, method=2, plot=False)
 
 ##
 #check return object
-def test_training(expectation):
-    with expectation:
-        assert obj1.training() is not None
+def test_training():
+    assert obj1.training() is not None
 ##
 
 #call the validate() function
-mean1, sig1, cov1 = obj1.validate(plot=True)
+mean1, sig1, cov1 = obj1.validate(plot=False)
 
 ##
 #check mean1, sig1, cov1
-def test_validate(expectation):
-    with expectation:
-        assert mean1 is not None 
-        assert sig1 is not None 
-        assert cov1 is not None 
+def test_validate():
+    assert mean1 is not None 
+    assert sig1 is not None 
+    assert cov1 is not None 
 ##
 
 #call plot_mix() to mix in the GP
@@ -78,34 +67,31 @@ mixed_mean, mixed_intervals = obj1.plot_mix(g, plot_fdagger=True, \
 
 ##
 #check returned variables
-def test_plot_mix(expectation):
-    with expectation:
-        assert mixed_mean is not None
-        assert mixed_intervals is not None
+def test_plot_mix():
+    assert mixed_mean is not None
+    assert mixed_intervals is not None
 ##
 
 #call subplot_mix() and plot no GP results next to GP results
 obj1.subplot_mix(g, GP_mean=mean1, GP_var=np.square(sig1))
 
 ##
-#check figure generation
-def test_subplot_mix(g, expectation):
-    with expectation:
-        assert obj1.subplot_mix(g, GP_mean=mean1, GP_var=np.square(sig1)) \
-            is not None
+#check NoneType object
+def test_subplot_mix():
+    assert obj1.subplot_mix(g, GP_mean=mean1, GP_var=np.square(sig1)) \
+        is None
 ##
 
 #calculate the Mahalanobis points
-md_g, md_mean, md_sig, md_cov = obj1.MD_set(pts=3, plot=True)
+md_g, md_mean, md_sig, md_cov = obj1.MD_set(pts=3, plot=False)
 
 ##
 #check each output
-def test_MD_set(expectation):
-    with expectation:
-        assert md_g is not None 
-        assert md_mean is not None 
-        assert md_sig is not None 
-        assert md_cov is not None 
+def test_MD_set():
+    assert md_g is not None 
+    assert md_mean is not None 
+    assert md_sig is not None 
+    assert md_cov is not None 
 ##
 
 #use the points to calculate the Mahalanobis distance for our GP
@@ -113,18 +99,16 @@ md_gp, md_ref = obj1.md_squared(md_g, md_mean, md_cov, n_curves=1000)
 
 ##
 #check return output
-def test_md_squared(expectation):
-    with expectation:
-        assert md_gp is not None 
-        assert md_ref is not None 
+def test_md_squared():
+    assert md_gp is not None 
+    assert md_ref is not None 
 ##
 
 #call 
 obj1.md_plotter(md_gp, md_ref, hist=True)
 
 ##
-#check plotter for axes generation
-def test_md_plotter(md_gp, md_ref, expectation):
-    with expectation:
-        assert obj1.md_plotter(md_gp, md_ref) is not None
+#check NoneType object
+def test_md_plotter():
+    assert obj1.md_plotter(md_gp, md_ref) is None
 ##
--- Tests/test_bivariate_BMM.py
+++ Tests/test_bivariate_BMM.py
@@ -1,17 +1,10 @@
 ###########################################################
-# Tutorial example for Bivariate BMM (simplified)
+# Tutorial example for Bivariate BMM 
 ###########################################################
 
 #import packages
 import numpy as np
-import math
-import pytest
 from contextlib import contextmanager
-import statistics
-from scipy import stats, special, integrate
-
-import matplotlib.pyplot as plt
-from matplotlib.ticker import AutoMinorLocator
 
 #matplotlib settings for Latex plots
 import matplotlib
@@ -21,7 +14,6 @@ matplotlib.rcParams.update({
 })
 
 #load SAMBA classes
-from samba.models import Models, Uncertainties
 from samba.discrepancy import Bivariate
 
 #set up linspace and expansions
@@ -35,18 +27,17 @@ model1 = Bivariate(ns, nl, error_model)
 
 ##
 #test class object
-@contextmanager
-def does_not_raise():
-    yield
+# @contextmanager
+# def does_not_raise():
+#     yield
 
-@pytest.mark.parametrize(
-    "ns, nl, error_model, expectation",
-    [(ns, nl, error_model, does_not_raise()),],
-)
+# @pytest.mark.parametrize(
+#     "ns, nl, error_model, expectation",
+#     [(ns, nl, error_model, does_not_raise()),],
+# )
 
-def test_Bivariate(ns, nl, error_model, expectation):
-    with expectation:
-        assert Bivariate(ns, nl, error_model) is not None
+def test_Bivariate():
+    assert Bivariate(ns, nl, error_model) is not None
 ##
 
 #call plot_models() from Models() class
@@ -54,15 +45,19 @@ model1.plot_models(g)
 
 ##
 #test ns, nl passing
-def test_class_variables(expectation):
-    with expectation:
-        assert Models.loworder == ns
-        assert Models.highorder == nl
+def test_class_variables():
+    if isinstance(ns, int) or isinstance(ns, float):
+        assert model1.loworder[0] == ns
+    else:
+        assert np.array_equal(model1.loworder, ns)
+    if isinstance(nl, int) or isinstance(nl, float):
+        assert model1.highorder[0] == nl
+    else:
+        assert np.array_equal(model1.highorder, nl)
 
-#check figure generation
-def test_plot_models(g, expectation):
-    with expectation:
-        assert model1.plot_models(g) is not None
+#check NoneType object
+def test_plot_models():
+    assert model1.plot_models(g) is None
 ##
 
 #call plot_mix() function to mix
@@ -70,8 +65,7 @@ mean1, intervals1 = model1.plot_mix(g, plot_fdagger=True)
 
 ##
 #check mean and interval
-def test_plot_mix(ns, nl, error_model, expectation):
-    with expectation:
-        assert mean1 is not None
-        assert intervals1 is not None
+def test_plot_mix():
+    assert mean1 is not None
+    assert intervals1 is not None
 ##
