0e2d6f011f02e66fe5185eecb776bf2896ebc4ca
Alexandra Semposki <as727414@ohio.edu>
2022-07-10 19:14:20 -0400

    added version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=['SAMBA'],
     # cmdclass={'build_ext': CustomBuildExtCommand},
     # ext_modules=ext_modules,
-    version='0.1',
+    version='0.1.0',
     description='Sandbox for testing methods of Bayesian Model Mixing',
     author='Alexandra Semposki',
     author_email='as727414@ohio.edu',
